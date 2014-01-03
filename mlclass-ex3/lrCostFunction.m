df180e60a1754592aa43c34858c8e3fe20f22bbc
rlopezc <Raul Lopez Cardenas>
2014-01-04 00:22:05 +0100

    Machine Learning Solution - Exercise 3

--- mlclass-ex3/lrCostFunction.m
+++ mlclass-ex3/lrCostFunction.m
@@ -5,48 +5,29 @@ function [J, grad] = lrCostFunction(theta, X, y, lambda)
 %   theta as the parameter for regularized logistic regression and the
 %   gradient of the cost w.r.t. to the parameters. 
 
-% Initialize some useful values
-m = length(y); % number of training examples
+    % Initialize some useful values
+    m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+    % You need to return the following variables correctly 
+    J = 0;
+    grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Hint: The computation of the cost function and gradients can be
-%       efficiently vectorized. For example, consider the computation
-%
-%           sigmoid(X * theta)
-%
-%       Each row of the resulting matrix will contain the value of the
-%       prediction for that example. You can make use of this to vectorize
-%       the cost function and gradient computations. 
-%
-% Hint: When computing the gradient of the regularized cost function, 
-%       there're many possible vectorized solutions, but one solution
-%       looks like:
-%           grad = (unregularized gradient for logistic regression)
-%           temp = theta; 
-%           temp(1) = 0;   % because we don't add anything for j = 0  
-%           grad = grad + YOUR_CODE_HERE (using the temp variable)
-%
+    % Hint: When computing the gradient of the regularized cost function, 
+    %       there're many possible vectorized solutions, but one solution
+    %       looks like:
+    %           grad = (unregularized gradient for logistic regression)
+    %           temp = theta; 
+    %           temp(1) = 0;   % because we don't add anything for j = 0  
+    %           grad = grad + YOUR_CODE_HERE (using the temp variable)
+    %
 
-
-
-
-
-
-
-
-
-
-% =============================================================
-
-grad = grad(:);
+    % compute Cost Function
+    sig = sigmoid(X * theta);
+    J = mean (- y .* log(sig) - (1 - y) .* log(1 - sig)) + sum(theta(2:end) .^2) * lambda / (2 * m);
+    
+    % compute Gradient
+    theta_temp = theta;
+    theta_temp(1) = 0;
+    grad = (X' * (sig - y) ./ m) + theta_temp * lambda / m;
 
 end
--- mlclass-ex3/predict.m
+++ mlclass-ex3/predict.m
@@ -3,33 +3,20 @@ function p = predict(Theta1, Theta2, X)
 %   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
 %   trained weights of a neural network (Theta1, Theta2)
 
-% Useful values
-m = size(X, 1);
-num_labels = size(Theta2, 1);
+    % Useful values
+    m = size(X, 1);
+    num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
+    % You need to return the following variables correctly 
+    p = zeros(size(X, 1), 1);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
-%               vector containing labels between 1 to num_labels.
-%
-% Hint: The max function might come in useful. In particular, the max
-%       function can also return the index of the max element, for more
-%       information see 'help max'. If your examples are in rows, then, you
-%       can use max(A, [], 2) to obtain the max for each row.
-%
-
-
-
-
-
-
-
-
-
-% =========================================================================
-
+    % compute hidden layer
+    X = [ones(m,1) X];
+    A = sigmoid(X * Theta1');
+    
+    % compute output layer
+    A = [ones(m,1) A];
+    h = sigmoid(A * Theta2');
+    [value, p] = max(h,[],2);
 
 end
