442aefc026d6e6deec9493672348003fe925594a
rlopezc <Raul Lopez Cardenas>
2014-01-07 17:24:14 +0100

    solution questions 3

--- mlclass-ex5/learningCurve.m
+++ mlclass-ex5/learningCurve.m
@@ -14,12 +14,12 @@ function [error_train, error_val] = ...
 %   datasets, you might want to do this in larger intervals.
 %
 
-% Number of training examples
-m = size(X, 1);
+    % Number of training examples
+    m = size(X, 1);
 
-% You need to return these values correctly
-error_train = zeros(m, 1);
-error_val   = zeros(m, 1);
+    % You need to return these values correctly
+    error_train = zeros(m, 1);
+    error_val   = zeros(m, 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return training errors in 
@@ -50,17 +50,12 @@ error_val   = zeros(m, 1);
 %           
 %       end
 %
+    
+    for i = 1:m
+        theta = trainLinearReg(X(1:i,:), y(1:i,:), lambda);
+        error_train(i) = linearRegCostFunction(X(1:i,:), y(1:i), theta, 0);
+        error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
+    end
 
-% ---------------------- Sample Solution ----------------------
-
-
-
-
-
-
-
-% -------------------------------------------------------------
-
-% =========================================================================
 
 end
