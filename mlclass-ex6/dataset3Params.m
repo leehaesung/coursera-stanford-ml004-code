1173e0cb6d2bd7d2c9095a9f4afb30d652d6d53f
rlopezc <Raul Lopez Cardenas>
2014-01-09 13:41:23 +0100

    solutions mlclass-ex6

--- mlclass-ex6/dataset3Params.m
+++ mlclass-ex6/dataset3Params.m
@@ -7,11 +7,10 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %   sigma based on a cross-validation set.
 %
 
-% You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+    % You need to return the following variables correctly.
+    C = 1;
+    sigma = 0.3;
 
-% ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
@@ -21,14 +20,25 @@ sigma = 0.3;
 %
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
-%
 
+    C_temp = 0;
+    sigma_temp = 0;
+    error_temp = 100000;
 
-
-
-
-
-
-% =========================================================================
-
+    steps = [0.01, 0.03, 0.1, 0.3, 1.0, 1.3, 10, 30];
+    
+    for C_step = steps
+        for sigma_step = steps
+            model = svmTrain(X, y, C_step, @(x1, x2) gaussianKernel(x1, x2, sigma_step));
+            predictions = svmPredict(model, Xval);
+            predict_error = mean(double(predictions ~= yval));
+            if (predict_error <  error_temp)
+                C_temp = C_step;
+                sigma_temp = sigma_step;
+                error_temp = predict_error;
+            end
+        end
+    end
+    C = C_temp
+    sigma = sigma_temp
 end
--- mlclass-ex6/emailFeatures.m
+++ mlclass-ex6/emailFeatures.m
@@ -4,11 +4,11 @@ function x = emailFeatures(word_indices)
 %   x = EMAILFEATURES(word_indices) takes in a word_indices vector and 
 %   produces a feature vector from the word indices. 
 
-% Total number of words in the dictionary
-n = 1899;
+    % Total number of words in the dictionary
+    n = 1899;
 
-% You need to return the following variables correctly.
-x = zeros(n, 1);
+    % You need to return the following variables correctly.
+    x = zeros(n, 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return a feature vector for the
@@ -49,13 +49,9 @@ x = zeros(n, 1);
 %
 
 
+    for i = 1:length(word_indices)
+        x(word_indices(i)) = 1;
+    end
 
-
-
-
-
-
-% =========================================================================
-    
 
 end
